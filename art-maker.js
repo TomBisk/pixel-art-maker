9793597077e6aed01b3b3a09c181e3bc0adf158f
Tomasz Bochenek <bochenekt@gmail.com>
2018-01-06 13:11:53 +0100

    Corrects rgbToHex function and add some comments to code

--- art-maker.js
+++ art-maker.js
@@ -1,22 +1,42 @@
-// TODO: tools > color options should be disabled when canvas is off.
 
-// function to create pixel canvas
+/**
+* checks values of width and height inputs and activates
+* 'create' button if canvas width and height are setted
+*/
+function isSize() {
+	if ($('#canvas-height').val() > 0 && $('#canvas-width').val() > 0) {
+		$('#canvas-create').removeAttr('disabled');
+	} 
+}
+
+// event listeners to checks changes of canvas size values
+$('#canvas-height').on('keyup mouseup', isSize );
+$('#canvas-width').on('keyup mouseup', isSize );
+
+/**
+* creates pixel canvas
+*/
 function makeGrid() {
-	const gridWidth = $("#input-width").val();
-	const gridHeight = $('#input-height').val();
-	$('#pixel-canvas').children().remove();
+	const gridWidth = $("#canvas-width").val(); //assign width value
+	const gridHeight = $('#canvas-height').val(); //assign height value
+	$('#pixel-canvas').children().remove(); // remove exist canvas
+	// checks if both size-inputs have a value
 	if (gridWidth > 0 && gridHeight > 0) {
-		$('input').removeAttr('disabled');
-		$('sidebar td').css({'border-color': '#d4d4d4'});
-		gridOfFlag = true;
+		$('input').removeAttr('disabled'); //activate unactive options
+		$('div > h2').css({'color': '#fafafa'});
+		$('sidebar td').css({'border-color': '#d4d4d4', 'opacity': ''});
+		$('sidebar td').on('click', setColor);
+		gridOnFlag = true;
 		$('input.grid-on').css({'font-weight': 'bolder' }).val('grid on');
 	} else {
 		$('.grid-options input').attr('disabled', 'disabled');
 		$('#color-picker').attr('disabled', 'disabled');
-		
-		$('sidebar td').css({'border-color': '#6f78a9'});
-		
+		$('div > h2').css({'color': '#6f78a9'});
+		$('sidebar td').off('click', setColor);
+		$('sidebar td').css({'border-color': '#6f78a9', 'opacity': '0.4'});
+		$('input[type=submit]').attr('disabled', 'disabled');
 	}
+	// creates canvas with declared size
 	for (let i = 0; i < gridHeight; i++) {
 		$('#pixel-canvas').append('<tr></tr>');
 		for ( let j = 0; j < gridWidth; j++) {
@@ -28,52 +48,64 @@ function makeGrid() {
   	return;
 }
 
-$('#size-picker').submit(function(event) {
+
+// 
+$('#size-form').submit(function(event) {
 	makeGrid();
 });
 						
-// function to fill the grid-cells by picked color
-function paintGrid() {
+
+/**
+* fills the canvas cells by setted paint color
+* or clear if clicked again the same color
+*/
+function paintCanvas() {
   	const pickedColor = $('#color-picker').val();
   	const cellColor = $(this).css('background-color');
-	// function to change the hex color value from picker to rgb() value
-	function hexToRgb() {
-		$('.hex-rgb').css({'background-color': pickedColor});
-		const rgbValue = $('.hex-rgb').css('background-color');
-		return rgbValue;
-	}
-  	if (cellColor === hexToRgb()) {
+  	if (cellColor === hexToRgb(pickedColor)) {
 		$(this).css({'background-color': '#fff'});
 	} 	else {
 	  	$(this).css({'background-color': pickedColor});
 	}
 }
-
-$('main table').on('mousedown', 'td', paintGrid);
-$('input.grid-on').on('click', gridOf);
-//$('table').on('mousedown', 'td', pain tGrid);
+/////////////
 
 
-// flag to check on/off in gridOf function
-let gridOfFlag = true;
-//function gridOf, to on/of grid visibility
-function gridOf() {
-	if (gridOfFlag === true) {
+// event listener to paint canvas
+$('main table').on('mousedown', 'td', paintCanvas);
+
+// event listener to on/off canvas grid
+$('input.grid-on').on('click', gridOn);
+
+// flag to check on/off in gridOn function
+let gridOnFlag = true;
+
+/**
+* function gridOn, to on/off grid visibility
+*/
+function gridOn() {
+	if (gridOnFlag === true) {
 		$(this).css({'font-weight': 'normal'}).val('grid off');
 		$('main td').css({'border-style': 'none'});
 		$('.grid-style, .grid-color').attr('disabled', 'disabled');
-		gridOfFlag = false;
+		gridOnFlag = false;
 	} else {
 		$(this).css({'font-weight': 'bolder' }).val('grid on');
 		$('main td').css({'border-style': bdStyle});
 		$('.grid-style, .grid-color').removeAttr('disabled');
-		gridOfFlag = true;
+		gridOnFlag = true;
 	}
 }
+
+// event listener on grid style button
 $('input.grid-style').on('click', gridStyle);
+
 // variable to store default or setted gridStyle and to restore it when the grid is off and on again
 let bdStyle = 'solid';
-// function gridStyle(), to change canvas grid-lines style
+
+/** 
+*function gridStyle(), to change canvas grid-lines style
+*/
 function gridStyle() {
 	const style = $('main td').css('border-style');
 	switch(style) {
@@ -95,53 +127,88 @@ function gridStyle() {
 }
 
 
+//
 $('input.grid-color').on('change', gridColor);
+
 // variable to set default and store setted color of grid-lines
 let bdColor = '#000';
+
 // function gridColor to change color of canvas grid-lines
 function gridColor() {
 	bdColor = $('.grid-color').val();
 	$('main td').css({'border-color': bdColor});
 }
 
+///////
 // paint color listener to calling lastUsed function
 $('#color-picker').on('change', lastUsed);
 
-// color history array
-const colHist = ['#000000',1,1,1,1];
+// Last used colors history array
+const colHist = ['#000000','','','',''];
 
-// function to handling color history
+/**
+* Handling color history. Sets stored in colHist array colors
+* to history cells. Adds and removes used colors to colHist arrray
+*/
 function lastUsed() {
-	$('#a').css({'background-color': colHist[0]});
-	$('#b').css({'background-color': colHist[1]});
-	$('#c').css({'background-color': colHist[2]});
-	$('#d').css({'background-color': colHist[3]});
-	$('#e').css({'background-color': colHist[4]});
+	// rules to fills history cells by stored in colHist color values
+	$('#col-hist-1').css({'background-color': colHist[0]});
+	$('#col-hist-2').css({'background-color': colHist[1]});
+	$('#col-hist-3').css({'background-color': colHist[2]});
+	$('#col-hist-4').css({'background-color': colHist[3]});
+	$('#col-hist-5').css({'background-color': colHist[4]});
+	// adds last used color to history if it does not exist yet and 
+	// removes oldest color if history is full
 	if (!(colHist.includes($('#color-picker').val()))) { 
 			colHist.unshift($('#color-picker').val());
 			colHist.pop();
 	}
 }
 
-// color history listener
-$('sidebar td').on('click', setColor);
-
-// function to set paint color from color history
+/**
+* Sets paint color to color clicked in 'last used' history 
+* (if color exist) and do nothing if clicked history cell 
+* is empty (background-color value in rgba format). Selected
+* color, before is setted, must be converted from rgb to hex value
+* by rgbToHex function.
+*/
 function setColor() {
 	if (!($(this).css('background-color')).includes('rgba')) {
 	$('#color-picker').val(rgbToHex(($(this).css('background-color'))));	
 	} 
 }
 
-// function to convert rgb color value to hex color value
+/**
+*  Converts rgb color value to hex color value
+*@param {string} rgbValue - rgb color value
+*@returns {string} hex color value
+*/
 function rgbToHex(rgbValue) {
-	let rgbArr = rgbValue.slice(4, -1).split(/,\s/);
-	for (let i = 0; i < rgbArr.length; i++) {
-		rgbArr[i] = parseInt(rgbArr[i]).toString(16);
-		if (rgbArr[i].length < 2) {
-			rgbArr[i] = '0' + rgbArr[i];
+	// creates array with strings of each rgb color value
+	const rgbArray = rgbValue.slice(4, -1).split(/,\s/);
+	// converts strings in array to decimal and next to hex value 
+	for (let i = 0; i < rgbArray.length; i++) {
+		rgbArray[i] = parseInt(rgbArray[i]).toString(16);
+		// checks for correct 'two digits per color' value format 
+		if (rgbArray[i].length < 2) {
+			rgbArray[i] = '0' + rgbArray[i];
 		}
 	}
-	const hexValue = '#' + rgbArr.join('');
+	// creates correct hex color value
+	const hexValue = '#' + rgbArray.join('');
 	return hexValue;
+}
+
+/**
+* Converts hex color value to rgb color value
+*@param {string} hexValue - hex color value
+*@returns {string} rgb color value
+*/
+function hexToRgb(hexValue) {
+	const hexArray = hexValue.slice(1).match(/.{1,2}/g);
+	for (let i = 0; i <hexArray.length; i++) {
+	  hexArray[i] = parseInt(hexArray[i],16).toString(10);
+	}
+	const rgbValue = 'rgb(' + hexArray[0] + ', ' + hexArray[1] + ', ' + hexArray[2] + ')'
+	return rgbValue;
 }
